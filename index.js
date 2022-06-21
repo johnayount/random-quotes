d354bd3695e6a39ec25ceadea66e7768c4985542
Johnny B <johnyount953@gmail.com>
2022-06-20 21:25:08 -0500

    Motivational quote randomizer

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let randNum = Math.floor(Math.random(4))
+
+let quote = (randNum)=> {
+    switch (randNum) {
+        case 1:
+            return "You can do it Buddy!";
+        case 2:
+            return 'Everyday is a new opportunity to let someone down!';
+        case 3:
+            return 'If I were you, I would just go back home and try again tomorrow';
+        default: 
+         break
+    }}
+
+
+
+const motivation = (dayOfWeek) => {
+   return randNum
+}
+
+
+
+
+
